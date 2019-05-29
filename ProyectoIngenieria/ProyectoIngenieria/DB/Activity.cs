4ac30027f1435e195f7094cc06fcf48a118632c0
DESKTOP-FVM8UDG\Diana Bonilla <dianapamela3197@gmail.com>
2019-05-29 13:02:43 -0600

    cambios en cs

--- ProyectoIngenieria/ProyectoIngenieria/DB/Activity.cs
+++ ProyectoIngenieria/ProyectoIngenieria/DB/Activity.cs
@@ -11,7 +11,8 @@ namespace ProyectoIngenieria.DB
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Activity
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,9 +24,21 @@ namespace ProyectoIngenieria.DB
         }
     
         public int id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre")]
         public string name { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Descripción")]
         public string descripcion { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Hora de Entrada")]
         public System.DateTime start_date { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Hora de Salida")]
         public System.DateTime end_date { get; set; }
         public int photo_id { get; set; }
     
