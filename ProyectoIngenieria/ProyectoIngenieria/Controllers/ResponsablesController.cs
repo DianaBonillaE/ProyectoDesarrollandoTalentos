86feb11da5c9d67a9f26a93b17466c6e5286ce54
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-05-29 14:00:55 -0600

    Responsable
    Crud Responsable

--- ProyectoIngenieria/ProyectoIngenieria/Controllers/ResponsablesController.cs
+++ ProyectoIngenieria/ProyectoIngenieria/Controllers/ResponsablesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using ProyectoIngenieria.DB;
 
 namespace ProyectoIngenieria.Controllers
@@ -15,9 +16,11 @@ namespace ProyectoIngenieria.Controllers
         private ProyectoIngenieriaEntities db = new ProyectoIngenieriaEntities();
 
         // GET: Responsables
-        public ActionResult Index()
+        public ActionResult Index(int page = 1, int pageSize = 4)
         {
-            return View(db.Responsable.ToList());
+            List<Responsable> responsableList = db.Responsable.ToList();
+            PagedList<Responsable> model = new PagedList<Responsable>(responsableList, page, pageSize);
+            return View(model);
         }
 
         // GET: Responsables/Details/5
@@ -27,7 +30,10 @@ namespace ProyectoIngenieria.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Responsable responsable = db.Responsable.Find(id);
+            Responsable responsable = db.Responsable.Include(a => a.Student).ToList().Find(c => c.identification == id);
+
+            List<Student> students = responsable.Student.ToList();
+            ViewBag.students = students;
             if (responsable == null)
             {
                 return HttpNotFound();
@@ -38,6 +44,9 @@ namespace ProyectoIngenieria.Controllers
         // GET: Responsables/Create
         public ActionResult Create()
         {
+            List<Student> students = db.Student.ToList();
+            ViewBag.students = students;
+
             return View();
         }
 
@@ -46,13 +55,29 @@ namespace ProyectoIngenieria.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "identification,name,last_name,address,phone_number,description,email")] Responsable responsable)
+        public ActionResult Create([Bind(Include = "identification,name,last_name,address,phone_number,description,email")]
+        Responsable responsable, List<int> students, HttpPostedFileBase File, string nameFile)
         {
             if (ModelState.IsValid)
             {
+                if (students.Count > 0)
+                {
+                    System.Collections.IList listStudents = students;
+                    for (int i = 0; i < listStudents.Count; i++)
+                    {
+                        string id = "" + listStudents[i];
+                        Student student = db.Student.Find(id);
+                        responsable.Student.Add(student);
+                    }
+                }
+
+
+
+
+
                 db.Responsable.Add(responsable);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("/Index");
             }
 
             return View(responsable);
@@ -65,11 +90,13 @@ namespace ProyectoIngenieria.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Responsable responsable = db.Responsable.Find(id);
+            Responsable responsable = db.Responsable.Include(a => a.Student).ToList().Find(c => c.identification == id);
             if (responsable == null)
             {
                 return HttpNotFound();
             }
+            List<Student> students = responsable.Student.ToList();
+            ViewBag.students = students;
             return View(responsable);
         }
 
@@ -78,14 +105,33 @@ namespace ProyectoIngenieria.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "identification,name,last_name,address,phone_number,description,email")] Responsable responsable)
+        public ActionResult Edit([Bind(Include = "identification,name,last_name,address,phone_number,description,email")] Responsable responsable, List<int> students)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(responsable).State = EntityState.Modified;
+
+                Responsable responsab1e = db.Responsable.Include(a => a.Student).ToList().Find(c => c.identification == responsable.identification);
+                responsab1e.Student.Clear();
+                if (students.Count > 0)
+                {
+                    System.Collections.IList listStudents = students;
+                    for (int i = 0; i < listStudents.Count; i++)
+                    {
+                        string id = "" + listStudents[i];
+                        Student student = db.Student.Find(id);
+                        responsable.Student.Add(student);
+                    }
+                }
+
+
+
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("/Index");
             }
+
+            List<Student> studentsList = db.Student.ToList();
+            ViewBag.students = studentsList;
             return View(responsable);
         }
 
@@ -96,7 +142,11 @@ namespace ProyectoIngenieria.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Responsable responsable = db.Responsable.Find(id);
+            Responsable responsable = db.Responsable.Include(a => a.Student).ToList().Find(c => c.identification == id);
+
+            List<Student> students = responsable.Student.ToList();
+            ViewBag.students = students;
+
             if (responsable == null)
             {
                 return HttpNotFound();
@@ -109,10 +159,21 @@ namespace ProyectoIngenieria.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
         {
-            Responsable responsable = db.Responsable.Find(id);
+            Responsable responsable = db.Responsable.Include(a => a.Student).ToList().Find(c => c.identification == id);
+
+            responsable.Student.Clear();
+
+
             db.Responsable.Remove(responsable);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+
+            }
+            return RedirectToAction("/Index");
         }
 
         protected override void Dispose(bool disposing)
