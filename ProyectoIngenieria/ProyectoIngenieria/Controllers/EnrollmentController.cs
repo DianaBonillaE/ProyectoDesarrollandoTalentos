6c1aaa896643808f01283bee95aa2dda1613a2c7
KoraNRG02 <koranrg02@gmail.com>
2019-06-18 21:15:56 -0600

    Matricula incompleto

--- ProyectoIngenieria/ProyectoIngenieria/Controllers/EnrollmentController.cs
+++ ProyectoIngenieria/ProyectoIngenieria/Controllers/EnrollmentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PagedList;
+using ProyectoIngenieria.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,64 @@ namespace ProyectoIngenieria.Controllers
 {
     public class EnrollmentController : Controller
     {
-        // GET: Enrollment
-        public ActionResult Index()
+        private ProyectoIngenieriaEntities db = new ProyectoIngenieriaEntities();
+
+        // GET: Curses
+        public ActionResult IndexEnrollment(int page = 1, int pageSize = 4)
         {
+            List<Curse> curseList = db.Curse.ToList();
+            PagedList<Curse> model = new PagedList<Curse>(curseList, page, pageSize);
+            return View(model);
+        }
+
+        // GET: Curses/Enrollment
+        public ActionResult ViewEnrollment(int? id)
+        {
+            Curse course = db.Curse.Find(id);
+            ViewBag.name = course.name;
+
+            List<Curse_Student> courseStudentList = db.Curse_Student.ToList();
+            List<Student> mostrarCourseStudentList = new List<Student>();
+
+            for (int i = 0; i < courseStudentList.Count; i++)
+            {
+
+                if (id.Equals(courseStudentList[i].curse_id))
+                {
+                    var idStudent = courseStudentList[i].student_identification;
+                    Student student = db.Student.Find(idStudent);
+                    mostrarCourseStudentList.Add(student);
+                }
+            }
+
+            ViewBag.students = mostrarCourseStudentList;
+
+            return View();
+
+        }
+
+        public ActionResult CreateEnrollment(int? id)
+        {
+            Curse course = db.Curse.Find(id);
+            ViewBag.name = course.name;
+            ViewBag.fechaFinalizacion = course.end_date;
+
+            List<Curse_Student> courseStudentList = db.Curse_Student.ToList();
+            List<Student> mostrarCourseStudentList = new List<Student>();
+
+            for (int i = 0; i < courseStudentList.Count; i++)
+            {
+
+                if (id != courseStudentList[i].curse_id)
+                {
+                    var idStudent = courseStudentList[i].student_identification;
+                    Student student = db.Student.Find(idStudent);
+                    mostrarCourseStudentList.Add(student);
+                }
+            }
+
+            ViewBag.students = mostrarCourseStudentList;
+
             return View();
         }
     }
