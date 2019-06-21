8aacdc56fc3a92c4338dd3a22b42e40de6de0489
DESKTOP-FVM8UDG\Diana Bonilla <dianapamela3197@gmail.com>
2019-06-21 13:01:52 -0600

    cursos cambios en las validaciones

--- ProyectoIngenieria/ProyectoIngenieria/Controllers/CursesController.cs
+++ ProyectoIngenieria/ProyectoIngenieria/Controllers/CursesController.cs
@@ -16,8 +16,12 @@ namespace ProyectoIngenieria.Controllers
         private ProyectoIngenieriaEntities db = new ProyectoIngenieriaEntities();
 
         // GET: Curses
-        public ActionResult Index(int page = 1, int pageSize = 4)
+        public ActionResult Index(string message, int page = 1, int pageSize = 4)
         {
+            if (message != null)
+            {
+                ViewBag.message = message;
+            }
             List<Course> curseList = db.Course.ToList();
             PagedList<Course> model = new PagedList<Course>(curseList, page, pageSize);
             return View(model);
@@ -41,59 +45,159 @@ namespace ProyectoIngenieria.Controllers
         // GET: Curses/Create
         public ActionResult Create()
         {
-            
+
+            ViewBag.rooms = db.Room.ToList();
+            ViewBag.teachers = db.Teacher.ToList();
+
             return View();
         }
 
         // POST: Curses/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,description,name,start_date,end_date")] Course curse)
+        public ActionResult Create([Bind(Include = "id,description,name,start_date,end_date")] Course course, List<int> rooms, List<string> teacher)
         {
+
             if (ModelState.IsValid)
             {
-                db.Course.Add(curse);
+
+                //validation of rooms
+                if (rooms != null)
+                {
+                    if (rooms.Count > 0)
+                    {
+                        System.Collections.IList listRooms = rooms;
+                        for (int i = 0; i < listRooms.Count; i++)
+                        {
+                            string id = "" + listRooms[i];
+                            Room room = db.Room.Find(Int32.Parse(id));
+                            course.Room.Add(room);
+                        }
+                    }
+                }
+                else
+                {
+                    //refill the lists
+                    ViewBag.rooms = db.Room.ToList();
+                    ViewBag.teachers = db.Teacher.ToList();
+                    //add the teachers in the course if the teachers list is different of null
+                    if (teacher != null)
+                    {
+                        System.Collections.IList listTeachers = teacher;
+                        for (int i = 0; i < listTeachers.Count; i++)
+                        {
+                            string id = "" + listTeachers[i];
+                            Teacher teacherDB = db.Teacher.Find(id);
+                            course.Teacher.Add(teacherDB);
+                        }
+
+                    }
+                    //message
+                    ViewBag.message = "Debe seleccionar al menos un aula y un profesor";
+                    return View(course);
+                }
+                //if the rooms list is different of null go to verify the list of teachers
+                if (teacher != null)
+                {
+                    System.Collections.IList listTeachers = teacher;
+                    for (int i = 0; i < listTeachers.Count; i++)
+                    {
+                        string id = "" + listTeachers[i];
+                        Teacher teacherDB = db.Teacher.Find(id);
+                        course.Teacher.Add(teacherDB);
+                    }
+                }
+                //if the list of teachers is null so show the message
+                else
+                {
+                    ViewBag.rooms = db.Room.ToList();
+                    ViewBag.teachers = db.Teacher.ToList();
+                    ViewBag.message = "Debe seleccionar al menos un profesor y un aula";
+                    return View(course);
+                }
+
+                //if everything is correct so add the curse
+                db.Course.Add(course);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { message = "El curso se agregó exitosamente" });
             }
 
-            
-            return View(curse);
+
+            return View(course);
         }
 
         // GET: Curses/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(string message, int? id)
         {
+            if (message != null)
+            {
+                ViewBag.message = message;
+            }
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Course curse = db.Course.Find(id);
-            if (curse == null)
+            Course course = db.Course.Include(a => a.Teacher).Include(a => a.Room).ToList().Find(a => a.id == id);
+            if (course == null)
             {
                 return HttpNotFound();
             }
-          
-            return View(curse);
+
+            ViewBag.rooms = db.Room.ToList();
+            ViewBag.teachers = db.Teacher.ToList();
+            return View(course);
         }
 
         // POST: Curses/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,description,name,start_date,end_date")] Course curse)
+        public ActionResult Edit([Bind(Include = "id,description,name,start_date,end_date")] Course curse, List<int> rooms, List<string> teacher)
         {
+
             if (ModelState.IsValid)
             {
                 db.Entry(curse).State = EntityState.Modified;
+                Course course = db.Course.Include(a => a.Teacher).Include(a => a.Room).ToList().Find(a => a.id == curse.id);
+
+                if (rooms != null)
+                {
+                    course.Room.Clear();
+                    if (rooms.Count > 0)
+                    {
+                        System.Collections.IList listRooms = rooms;
+                        for (int i = 0; i < listRooms.Count; i++)
+                        {
+                            string id = "" + listRooms[i];
+                            Room room = db.Room.Find(Int32.Parse(id));
+                            course.Room.Add(room);
+                        }
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("Edit/" + curse.id, new { message = "El curso debe tener aulas asociadas" });
+
+                }
+                if (teacher != null)
+                {
+                    course.Teacher.Clear();
+                    System.Collections.IList listTeachers = teacher;
+                    for (int i = 0; i < listTeachers.Count; i++)
+                    {
+                        string id = "" + listTeachers[i];
+                        Teacher teacherDB = db.Teacher.Find(id);
+                        course.Teacher.Add(teacherDB);
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("Edit/" + curse.id, new { message = "El curso debe tener profesores asociados" });
+                }
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { message = "El curso se actualizó exitosamente" });
             }
-            
-            return View(curse);
+
+            return RedirectToAction("Index");
         }
 
         // GET: Curses/Delete/5
@@ -108,6 +212,9 @@ namespace ProyectoIngenieria.Controllers
             {
                 return HttpNotFound();
             }
+
+            ViewBag.rooms = curse.Room.ToList();
+            ViewBag.teachers = curse.Teacher.ToList();
             return View(curse);
         }
 
@@ -117,6 +224,10 @@ namespace ProyectoIngenieria.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             Course curse = db.Course.Find(id);
+
+            curse.Room.Clear();
+            curse.Teacher.Clear();
+
             db.Course.Remove(curse);
             db.SaveChanges();
             return RedirectToAction("Index");
